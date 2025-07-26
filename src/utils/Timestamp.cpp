bd6f62dfa88b2c10d53edb941604ca66262725a2
JALAJ-PATEL <jpatel.pvt@gmail.com>
2025-07-26 18:49:27 +0530

    Timestamping

--- src/utils/Timestamp.cpp
+++ src/utils/Timestamp.cpp
@@ -5,8 +5,12 @@
 #include <iomanip>
 
 std::string Timestamp::getCurrentUTCTime() {
-    auto now = std::chrono::system_clock::now();
+    auto now = std::chrono::system_clock::now(); // Captures the current time of system clock
+
+    // Convert to time_t for easier formatting
     std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+
+    // gmtime converts time_t to UTC time
     std::tm* gmt_time = std::gmtime(&now_time);
 
     std::ostringstream oss;
